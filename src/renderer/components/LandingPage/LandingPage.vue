9ac76eb02d468a61ce46429191605589a4cde26b
Praveen2710 <pbanthia@masonlive.gmu.edu>
2019-05-18 07:59:39 -0400

    error handling

--- src/renderer/components/LandingPage/LandingPage.vue
+++ src/renderer/components/LandingPage/LandingPage.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="wrapper">
-    <h1>{{connectionError}}</h1>
-    <form-page :form="form" @newRow='addNewRow' @updatedRow='findRowPosition'></form-page>
-    <major-list :rows="rows" @editRow='loadRowToEdit'></major-list>
+    <div v-if="connectionError !== ''">
+       <h1 class="error">{{connectionError}}</h1>
+    </div>
+    <div v-else> 
+      <form-page :form="form" @newRow='addNewRow' @updatedRow='findRowPosition'></form-page>
+      <major-list :rows="rows" @editRow='loadRowToEdit'></major-list>
+    </div>
   </div>
 </template>
 
@@ -318,64 +322,8 @@
     padding: 60px 80px;
     width: 100vw;
   }
-
-  #logo {
-    height: auto;
-    margin-bottom: 20px;
-    width: 420px;
-  }
-
-  main {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  main > div { flex-basis: 50%; }
-
-  .left-side {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
-  }
-
-  .title {
-    color: #2c3e50;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
-
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-
-  .doc p {
-    color: black;
-    margin-bottom: 10px;
-  }
-
-  .doc button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
-  }
-
-  .doc button.alt {
-    color: #42b983;
-    background-color: transparent;
+  
+  .error {
+    color: red
   }
 </style>
